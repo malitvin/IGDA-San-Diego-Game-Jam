b3ab9db842220bf3eb23eb7604a399a89b2d4c29
Maximus <malitvin@ncsu.edu>
2018-09-30 14:07:51 -0400

    enemy death and hud init

---
+++ GameJamUnity/Assets/Src/Game/UI/HUD/HUDController.cs
@@ -0,0 +1,25 @@
+﻿//Unity
+using UnityEngine;
+
+namespace UI.HUD
+{
+    public class HUDController : BaseController
+    {
+        public HUDController()
+        {
+            viewFactory.CreateAsync<HUDView>("GUI/PlayerHUD", (v) =>
+            {
+                Debug.Log("HERE");
+                view = v;
+                OnCreationComplete();
+            });
+        }
+
+        private HUDView _hudView { get { return view as HUDView; } }
+
+        private void OnCreationComplete()
+        {
+            _hudView.OnCreationComplete();
+        }
+    }
+}
---
+++ GameJamUnity/Assets/Src/Game/UI/HUD/HUDView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+using GhostGen;
+
+namespace UI.HUD
+{
+    public class HUDView : UIView
+    {
+        public CanvasGroup _waveGrid;
+        public TextMeshProUGUI _waveText;
+
+        public void OnCreationComplete()
+        {
+
+        }
+    }
+}
