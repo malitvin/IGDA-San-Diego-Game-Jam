dcd9adea99890e5890d371ad94b0c86e27c03022
Maximus <malitvin@ncsu.edu>
2018-09-30 05:28:45 -0400

    Wave Spawn Logic Complete

--- GameJamUnity/Assets/Src/Game/Gameplay/Flow/BetweenWaveState.cs
+++ GameJamUnity/Assets/Src/Game/Gameplay/Flow/BetweenWaveState.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BetweenWaveState : FlowState {
+public class BetweenWaveState : FlowState
+{
 
     public BetweenWaveState(MonsterGenerator generator, LevelConfig config) : base(generator, config)
     {
  
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        Singleton.instance.gui.screenFader.StartCoroutine(GoBackToKillingMonsters());
+    }
+
+    private IEnumerator GoBackToKillingMonsters()
+    {
+        yield return new WaitForSeconds(_generator._currentWave.waveCompleteWaitTime);
+        _generator.SetFlowState(State.Monster);
     }
 }
--- GameJamUnity/Assets/Src/Game/Gameplay/Flow/IFlowState.cs
+++ GameJamUnity/Assets/Src/Game/Gameplay/Flow/IFlowState.cs
@@ -7,4 +7,5 @@ public interface IFlowState  {
     void Enter();
     void OnUpdate();
     void Exit();
+    void OnMonsterDestroyed();
 }
